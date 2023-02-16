2fc62d4f89f30d647c8f0424a7df279f50beba22
Nataliacardenas <ncardenasrestrepo@gmail.com>
2023-02-16 08:16:20 -0500

    NT2 Se modifica Main con f prints y se crea archivo de condicionales

---
+++ main.py
@@ -0,0 +1,19 @@
+#Datos Primitivos Quemados
+nombreUsuario = "Santiago"
+edadUsuario = 19
+estaturaUsuario = 1.72
+yaDesayuno = True
+
+#direccionUsuario = input("Digita tu direccion ")
+
+#Salidas De Consola
+#print("Hola tu nombre es " + nombreUsuario)
+#print("Tu edad es: ", edadUsuario, " estas viejito")
+#print(f"tu direccion es {direccionUsuario} vive cerca")
+
+numeroUno = int(input("Digite un numero "))
+numeroDos = int(input("Digite un numero "))
+
+sumatoria = numeroUno + numeroDos
+
+print(sumatoria)
